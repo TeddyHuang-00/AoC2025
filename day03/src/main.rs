7ca6af5bc0565a385341b65b9cc3f01f3ce11d36
Nan Huang <teddyhuangnan@gmail.com>
2025-12-07 21:41:29 -0500

    feat: Implementation solution for day 3

---
+++ day03/src/main.rs
@@ -0,0 +1,105 @@
+use anyhow::Result;
+use rayon::prelude::*;
+use util::{Solution, reader::parse_lines_from_file};
+
+type Bank = Vec<u32>;
+
+struct Puzzle {
+    banks: Vec<Bank>,
+}
+
+impl Puzzle {
+    fn parse_bank(input: &str) -> Result<Bank> {
+        input
+            .chars()
+            .map(|c| {
+                c.to_digit(10)
+                    .ok_or_else(|| anyhow::anyhow!("Failed to parse {c} as digit"))
+            })
+            .collect::<Result<Bank>>()
+    }
+
+    fn new(example: bool) -> Result<Self> {
+        let banks = parse_lines_from_file(Self::DAY, example, Self::parse_bank)?;
+        Ok(Self { banks })
+    }
+}
+
+impl Solution for Puzzle {
+    const DAY: u8 = 3;
+
+    /// For each bank, find the largest digit in the bank[:-1] so that there is
+    /// at least one digit after it, then find the largest digit after it.
+    /// Put them together and sum the results for all banks.
+    fn part1(&self) -> String {
+        self.banks
+            .par_iter()
+            .map(|bank| {
+                // Find the largest digit in the bank[:-1]
+                let (idx, &first_digit) = bank[..bank.len() - 1]
+                    .iter()
+                    .enumerate()
+                    .reduce(|(l_idx, l_val), (idx, val)| {
+                        if val > l_val {
+                            (idx, val)
+                        } else {
+                            (l_idx, l_val)
+                        }
+                    })
+                    .unwrap_or_else(|| unreachable!("Bank should have at least two digits"));
+                // Find the largest digit in the bank after idx
+                let second_digit = bank[idx + 1..].iter().max().unwrap_or_else(|| {
+                    unreachable!("At least one digit should be after the largest digit")
+                });
+                first_digit * 10 + second_digit
+            })
+            .sum::<u32>()
+            .to_string()
+    }
+
+    /// For each bank, use dynamic programming to find the largest 12-digit
+    /// number that can be formed by the digits in the bank while maintaining
+    /// their order.
+    fn part2(&self) -> String {
+        self.banks
+            .par_iter()
+            .map(|bank| {
+                bank.iter().fold(vec![0u64; 13], |mut dp, &digit| {
+                    for len in (1..=12).rev() {
+                        // At each position, either take the digit or not
+                        dp[len] = dp[len].max(dp[len - 1] * 10 + u64::from(digit));
+                    }
+                    dp
+                })[12]
+            })
+            .sum::<u64>()
+            .to_string()
+    }
+}
+
+fn main() -> Result<()> {
+    let puzzle = Puzzle::new(false)?;
+    println!("Day {} Part 1: {}", Puzzle::DAY, puzzle.part1());
+    println!("Day {} Part 2: {}", Puzzle::DAY, puzzle.part2());
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() -> Result<()> {
+        let puzzle = Puzzle::new(true)?;
+        assert_eq!(puzzle.part1(), "357");
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<()> {
+        let puzzle = Puzzle::new(true)?;
+        assert_eq!(puzzle.part2(), "3121910778619");
+        Ok(())
+    }
+}
