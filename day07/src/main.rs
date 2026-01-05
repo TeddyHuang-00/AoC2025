f85200cad242e54ac984246706a1a9625612fc41
Nan Huang <teddyhuangnan@gmail.com>
2026-01-06 00:05:55 +0800

    perf: Use simplified BFS and avoid precomputation

--- day07/src/main.rs
+++ day07/src/main.rs
@@ -1,62 +1,45 @@
-use std::collections::{BTreeMap, BTreeSet};
+use std::collections::BTreeSet;
 
 use anyhow::Result;
-use ndarray::{Zip, parallel::prelude::*, prelude::*};
+use ndarray::parallel::prelude::*;
 use rayon::prelude::*;
 use util::{
     Solution,
     reader::{parse_char_grid, read_file},
 };
 
-#[derive(Clone, Copy)]
-enum Grid {
-    Empty,
-    Start,
-    Splitter,
-}
+type Position = (usize, usize);
 
 struct Puzzle {
-    start: (usize, usize),
-    /// Step distance to next splitter in downward direction.
-    ///
-    /// This is intended to speed up traversal, as we can skip over empty cells
-    /// in one step.
-    ///
-    /// We may also transform this into a graph for the whole grid, but this is
-    /// simpler at the cost of some more runtime memory.
-    shortcut: Array2<usize>,
+    start: Position,
+    /// Positions of splitters in row-major order
+    splitters: BTreeSet<Position>,
+    /// Width of the grid, used to initialize beam states
+    width: usize,
 }
 
 impl Puzzle {
     fn new(example: bool) -> Result<Self> {
         let grid = parse_char_grid(read_file(Self::DAY, example)?, |c| match c {
-            '.' => Ok(Grid::Empty),
-            'S' => Ok(Grid::Start),
-            '^' => Ok(Grid::Splitter),
+            '.' | 'S' | '^' => Ok(c),
             _ => anyhow::bail!("Invalid character in grid: {c}"),
         })?;
         let start = grid
             .indexed_iter()
             .par_bridge()
-            .find_map_any(|((r, c), &v)| (matches!(v, Grid::Start)).then_some((r, c)))
+            .find_map_any(|((r, c), &v)| (matches!(v, 'S')).then_some((r, c)))
             .ok_or_else(|| anyhow::anyhow!("No start position found in grid"))?;
-        let mut shortcut = Array2::zeros((grid.nrows(), grid.ncols()));
-        Zip::from(shortcut.lanes_mut(Axis(0)))
-            .and(grid.lanes(Axis(0)))
-            .par_for_each(|mut shortpass, lane| {
-                let mut next_splitter = 0;
-                for (s, c) in shortpass.iter_mut().zip(lane.iter()).rev() {
-                    match c {
-                        // Reset counter at splitter
-                        Grid::Splitter => next_splitter = 0,
-                        // Increase distance at empty or start
-                        _ => next_splitter += 1,
-                    }
-                    *s = next_splitter;
-                }
-            });
-
-        Ok(Self { start, shortcut })
+        let splitters = grid
+            .indexed_iter()
+            .par_bridge()
+            .filter_map(|((r, c), &v)| (matches!(v, '^')).then_some((r, c)))
+            .collect();
+        let (_, width) = grid.dim();
+        Ok(Self {
+            start,
+            splitters,
+            width,
+        })
     }
 }
 
@@ -67,64 +50,57 @@ impl Solution for Puzzle {
         Self::new(example).unwrap_or_else(|e| panic!("Failed to parse input: {e}"))
     }
 
-    /// To find all splitters along the path, we can do a depth-first search
-    /// from the start position, keeping track of all visited positions
-    /// (splitters), and let frontiers be the start for the next beam.
-    ///
-    /// BFS would also work, they just differ in the order of visiting nodes.
+    /// To find all splitters along the path, we can just iterate over the
+    /// splitters by row order. If there is an active beam at the column of the
+    /// splitter, it is activated, and we update the state of the beams
+    /// accordingly.
     fn part1(&self) -> String {
-        let width = self.shortcut.ncols();
-        let height = self.shortcut.nrows();
-        let mut visited = BTreeSet::new();
-        let mut frontier = vec![self.start];
-        while let Some((r, c)) = frontier.pop() {
-            let nr = r + self.shortcut[[r, c]];
-            if nr >= height || !visited.insert((nr, c)) {
-                continue;
-            }
-            [-1, 1]
+        let mut beams = vec![false; self.width];
+        beams[self.start.1] = true;
+        let (_, count) =
+            self.splitters
                 .iter()
-                .filter_map(|&side| {
-                    let nc = c.wrapping_add_signed(side);
-                    (nc < width).then_some((nr, nc))
-                })
-                .for_each(|pos| frontier.push(pos));
-        }
-        visited.len().to_string()
+                .fold((beams, 0), |(mut beams, mut count), &(_, c)| {
+                    if beams[c] {
+                        // Found a splitter along an active beam
+                        count += 1;
+                        beams[c] = false;
+                        if let Some(v) = beams.get_mut(c.wrapping_add_signed(-1)) {
+                            *v = true;
+                        }
+                        if let Some(v) = beams.get_mut(c + 1) {
+                            *v = true;
+                        }
+                    }
+                    (beams, count)
+                });
+        count.to_string()
     }
 
-    /// Similar to part 1, but we additionally keep track of the number of ways
-    /// to reach each position in the frontier. When we reach the bottom row,
-    /// those are counts of unique paths reaching the bottom through that
-    /// position. We sum those counts to get the total number of unique paths to
-    /// the bottom.
+    /// Similar to part 1, but we instead keep track of the number of beams to
+    /// reach each column. When we reach the bottom row, those are counts of
+    /// unique paths reaching the bottom through that position. We sum those
+    /// counts to get the total number of unique paths to the bottom.
     fn part2(&self) -> String {
-        let width = self.shortcut.ncols();
-        let height = self.shortcut.nrows();
-        let mut count = 0usize;
-        let mut frontier = vec![(self.start, 1)];
-        while !frontier.is_empty() {
-            let mut next_layer = BTreeMap::new();
-            for ((r, c), n) in frontier {
-                let nr = r + self.shortcut[[r, c]];
-                if nr >= height {
-                    // Reached bottom row, add to count
-                    count += n;
-                    continue;
+        let mut beams = vec![0; self.width];
+        beams[self.start.1] = 1;
+        let beams = self.splitters.iter().fold(beams, |mut beams, &(_, c)| {
+            match beams[c] {
+                0 => beams,
+                cnt => {
+                    // Found a splitter along an active beam
+                    if let Some(v) = beams.get_mut(c.wrapping_add_signed(-1)) {
+                        *v += cnt;
+                    }
+                    if let Some(v) = beams.get_mut(c + 1) {
+                        *v += cnt;
+                    }
+                    beams[c] = 0;
+                    beams
                 }
-                [-1, 1]
-                    .iter()
-                    .filter_map(|&side| {
-                        let nc = c.wrapping_add_signed(side);
-                        (nc < width).then_some((nr, nc))
-                    })
-                    .for_each(|pos| {
-                        next_layer.entry(pos).and_modify(|e| *e += n).or_insert(n);
-                    });
             }
-            frontier = next_layer.into_iter().collect();
-        }
-        count.to_string()
+        });
+        beams.into_iter().sum::<u64>().to_string()
     }
 }
 
